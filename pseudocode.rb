de7f8509f19a92cd663a4f123c759d4bd401c737
rongxanh88 <rongxanh88@gmail.com>
2017-03-14 09:09:02 -0600

    first commit

---
+++ pseudocode.rb
@@ -0,0 +1,14 @@
+Terminal asks answers phone with a phrase. Then the program captures the response.
+
+If the response is empty (user just hit enter), reply back with , "Hello"
+
+if the response has any lowercase letters, reply back with, "I am having a hard time hearing you"
+
+if the response has all caps, reply back with, "No this is not a grocery store"
+
+if the response is "GOODBYE", increment a "goodbye" counter by one (start from zero). If the counter is equal to one,
+    reply back with , "Is there anything I can help you with".
+
+else if counter is 2, reply back and close the program.
+    
+Create a loop with the above program, so that it keeps requesting input from user.
