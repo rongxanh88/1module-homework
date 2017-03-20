058260b750e948bf3fc17ede8e9289867b61865c
rongxanh88 <rongxanh88@gmail.com>
2017-03-20 11:05:20 -0600

    First commit for new calculator

---
+++ calculator/test/calculator_test.rb
@@ -0,0 +1,30 @@
+gem 'minitest'
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require "../lib/calculator.rb"
+
+class CalculatorTest < Minitest::Test
+
+  def test_instance
+
+  end
+  
+  def test_total
+
+  end
+  
+  def test_add
+
+  end
+  
+  def test_clear
+
+  end
+  
+  def test_subtract
+
+  end
+  
+
+end
