73319cae6a138a2b7beef7ccd003598fc87623ce
rongxanh88 <rongxanh88@gmail.com>
2017-03-26 16:53:35 -0600

    finished .one? tests

--- enums/one_test.rb
+++ enums/one_test.rb
@@ -16,35 +16,48 @@ class OneTest < Minitest::Test
     ornaments = ["bracelet", "anklet", "earring"]
     exactly_one_ring = ornaments.one? do |ornament|
       # Your code goes here
+      ornament == "ring"
     end
     refute exactly_one_ring
   end
 
   def test_not_just_one_ring
-    skip
     ornaments = ["bracelet", "ring", "ring", "anklet", "earring"]
     # Your code goes here
+    exactly_one_ring = ornaments.one? do |ornament|
+      ornament == "ring"
+    end
+    
     refute exactly_one_ring
   end
 
   def test_one_time
-    skip
     words = ["morning", "time", "evening", "noon", "dusk", "dawn"]
     # Your code goes here
+    exactly_one_time = words.one? do |word|
+      word == "time"
+    end
+    
     assert exactly_one_time
   end
 
   def test_one_double_digit_number
-    skip
     numbers = [8, 2, 10, 333, 9, 101]
     # Your code goes here
+    exactly_one_double_digit = numbers.one? do |num|
+      num.to_s.split("").to_a.length == 2
+    end
+    
     assert exactly_one_double_digit
   end
 
   def test_not_one_even_number
-    skip
     numbers = [3, 20, 81, 10, 391, 32]
     # Your code goes here
+    exactly_one_even_number = numbers.one? do |num|
+      num % 2 ==0
+    end
+    
     refute exactly_one_even_number
   end
 
